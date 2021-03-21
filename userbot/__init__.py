bc9a20a371174e9f75b79c6b0b191ed0d4307e69
ronaldolarenza <79603045+ronaldolarenza@users.noreply.github.com>
2021-03-21 08:13:45 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -100,7 +100,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/ronaldolarenza/Space-Bot.git")
+    "https://github.com/ronaldolarenza/AZNERAL-BOT.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "Kampang")
 
