19a12ccce8aaa141b88a7e9414ccedd99c42a0ae
ronaldolarenza <79603045+ronaldolarenza@users.noreply.github.com>
2021-03-21 07:54:09 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"BOT-ANGKASA 🌐 V{BOT_VER} [ALHAMDULILLAH MENYALA 🔥]")
+    f"BOT-AZNERAL🥵 V{BOT_VER} [ALHAMDULILLAH MENYALA JHON🥵🔥]")
 
 
 if len(argv) not in (1, 3, 4):
