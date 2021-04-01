22b2a05dcd64b6840bfe13ec3a042156bbda2a04
ronaldolarenza <79603045+ronaldolarenza@users.noreply.github.com>
2021-04-01 11:06:46 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -120,7 +120,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "✦✧ ᴏᴡɴᴇʀ ✧✦:[RONALDO LARENZA](https://t.me/hirascal)\n \n✦✧ ʀᴇᴘᴏsɪᴛᴏʀʏ ✧✦:[AZNERAL-BOT](https://github.com/ronaldolarenza/AZNERAL-BOT) \n \n✦✧ ᴄʜᴀɴɴᴇʟ ✧✦:[SUPPORT](https://t.me/kataazneral)\n     \n"
+        "✦✧ ᴏᴡɴᴇʀ ✧✦:[RONALDO LARENZA](https://t.me/hirascal)\n \n✦✧ ʀᴇᴘᴏsɪᴛᴏʀʏ ✧✦:[AZNERAL-BOT](https://github.com/ronaldolarenza/AZNERAL-BOT) \n \n✦✧ ᴄʜᴀɴɴᴇʟ ✧✦:[SUPPORT](https://t.me/bestparrtt)\n     \n"
     )
 
 
